b5fb9f11e0240b8fcd1c46a9f0e778891b21d6c4
kozmod <kozmod@list.ru>
2020-03-19 21:54:33 +0300

    hiding variable test

---
+++ core/function/hiding_var_test.go
@@ -0,0 +1,25 @@
+package function
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+const one = 1
+const two = 2
+
+func TestHideVar(t *testing.T) {
+	x := one
+	fmt.Println(x) // 1
+	assert.Equal(t, one, x)
+	{
+		fmt.Println(x) // 1
+		assert.Equal(t, one, x)
+		x := 2
+		fmt.Println(x) // 2
+		assert.Equal(t, two, x)
+	}
+	fmt.Println(x) // 1
+	assert.Equal(t, one, x)
+}
