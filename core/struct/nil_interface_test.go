1cd183dc1030aa3b5d19f52072cc2862aea653d9
kozmod <kozmod@list.ru>
2020-03-19 21:50:54 +0300

    nil interface test

---
+++ core/struct/nil_interface_test.go
@@ -0,0 +1,58 @@
+package _struct
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+const nilVal = "0"
+const notNilVal = "hello"
+
+type i interface {
+	do()
+	val() string
+}
+
+type st struct {
+	str string
+}
+
+func (s *st) do() {
+	if s == nil {
+		fmt.Println("<nil>")
+		return
+	}
+	fmt.Println(s.str)
+}
+
+func (s *st) val() (val string) {
+	if s == nil {
+		return nilVal
+	}
+	return s.str
+}
+
+func Test1(t *testing.T) {
+	var i i
+	var s *st
+	assert.Nil(t, s)
+	assert.Nil(t, i)
+
+	i = s
+	assert.Nil(t, s)
+	assert.Nil(t, i)
+
+	describe(i)
+	i.do()
+	assert.Equal(t, nilVal, i.val())
+
+	i = &st{notNilVal}
+	describe(i)
+	i.do()
+	assert.Equal(t, notNilVal, i.val())
+}
+
+func describe(i i) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}
