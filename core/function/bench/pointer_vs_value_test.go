4f263f77a805dac5f21e2e5fb0c6f12e4cb0fb8f
18388871 <Koz00mod00>
2020-04-05 12:20:49 +0300

    add benchmark "pointer vs  value"

--- core/function/bench/pointer_vs_value_test.go
+++ core/function/bench/pointer_vs_value_test.go
@@ -1,14 +1,9 @@
 package bench
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"os"
-	"os/exec"
 	"runtime/trace"
-	"strings"
 	"testing"
 )
 
@@ -34,32 +29,9 @@ func byPointer() *S {
 	}
 }
 
-func TestBenchmarkMemoryStack(t *testing.T) {
-	cmd := exec.Command(
-		"go", "test", "./...",
-		"-bench=BenchmarkMemoryStack", "-benchmem", "-run=^$", "-count=10",
-	)
-
-	cmd.Stdin = strings.NewReader("bench")
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("\n %s", out.String())
-	if err := ioutil.WriteFile("out/stack.txt", out.Bytes(), 0644); err != nil {
-		log.Fatal(err)
-	}
-
-	outbanch, err := exec.Command("benchstat", "out/stack.txt").Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("benchstat outpur:\n%s", outbanch)
-}
-
-// go test ./... -bench=BenchmarkMemoryStack -benchmem -run=^$ -count=10 2>&1 | tee out/stack.txt && benchstat out/stack.txt 2>&1 | tee out/stack-bstat.txt
+/*
+go test ./... -bench=BenchmarkMemoryStack -benchmem -run=^$ -count=10 2>&1 | tee out/stack.txt && benchstat out/stack.txt 2>&1 | tee out/stack-bstat.txt
+*/
 func BenchmarkMemoryStack(b *testing.B) {
 	var s S
 
@@ -85,7 +57,9 @@ func BenchmarkMemoryStack(b *testing.B) {
 	_ = fmt.Sprintf("%v", s.a)
 }
 
-// go test ./... -bench=BenchmarkMemoryHeap -benchmem -run=^$ -count=10 2>&1 | tee out/head.txt && benchstat out/head.txt 2>&1 | tee out/head-bstat.txt
+/*
+go test ./... -bench=BenchmarkMemoryHeap -benchmem -run=^$ -count=10 2>&1 | tee out/head.txt && benchstat out/head.txt 2>&1 | tee out/head-bstat.txt
+*/
 func BenchmarkMemoryHeap(b *testing.B) {
 	var s *S
 
