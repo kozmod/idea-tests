a8cd226d543f0055ba2ed0c2aa5aca3a57c821c2
18388871 <Koz00mod00>
2020-04-22 10:34:49 +0300

    add data structure test

---
+++ core/datastruct/array_test.go
@@ -0,0 +1,16 @@
+package datastruct_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestArrayVsSlice(t *testing.T) {
+	t.Skip()
+	//a := [3]string{"a", "b", "c","d"}
+	s := []string{"a", "b", "c", "d"}
+	//a = append(a, "a")
+	s = append(s, "a")
+	//fmt.Println(len(a), len(a))
+	fmt.Println(cap(s), cap(s))
+}
