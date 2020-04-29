c69aa5293582984cb51e038c93a0f75d905aa052
18388871 <Koz00mod00>
2020-04-29 20:49:04 +0300

    add logic to nil_not_nil_test.go

--- core/errors/nil_not_nil_test.go
+++ core/errors/nil_not_nil_test.go
@@ -1,45 +1,63 @@
+/*
+Why is my nil error value not equal to nil?
+
+	https://golang.org/doc/faq#nil_error
+	https://stackoverflow.com/questions/31728656/why-is-err-nil
+	https://stackoverflow.com/questions/58272987/assigning-nil-value-to-driver-value-makes-driver-value-not-equal-nil
+	https://stackoverflow.com/questions/53892508/golang-returning-nil-does-not-return-nil
+	https://stackoverflow.com/questions/44900065/nil-pointer-to-struct-not-deep-equal-to-nil
+*/
 package errors
 
 import (
-	"github.com/stretchr/testify/assert"
-	"log"
+	"fmt"
 	"testing"
 )
 
 type customError struct{}
 
 // Error implements the error interface.
-func (c *customError) Error() string {
+func (*customError) Error() string {
 	return "Find the bug."
 }
 
 // fail returns nil values for both return types.
-func fail() ([]byte, *customError) {
+func failA() ([]byte, *customError) {
 	return nil, nil
 }
 
-func fail2() ([]byte, error) {
+func failB() ([]byte, error) {
 	return nil, nil
 }
 
-// Why is my nil error value not equal to nil?
-//https://golang.org/doc/faq#nil_error
-//https://stackoverflow.com/questions/31728656/why-is-err-nil
-//https://stackoverflow.com/questions/58272987/assigning-nil-value-to-driver-value-makes-driver-value-not-equal-nil
-//https://stackoverflow.com/questions/53892508/golang-returning-nil-does-not-return-nil
-//https://stackoverflow.com/questions/44900065/nil-pointer-to-struct-not-deep-equal-to-nil
 func TestNilOrNotNil(t *testing.T) {
 	var err error
-	if _, err = fail(); err != nil {
-		assert.Nil(t, err)
-		log.Println("Why did this fail?")
+
+	step := 1
+	if _, err = failA(); err != nil {
+		fmt.Printf("%d:`failA()` -> Why did this fail?: %v\n", step, err)
 	} else {
-		log.Println("`fail()` err is nil")
+		fmt.Printf("%d:`failA()` -> err: %v\n", step, err)
 	}
 
-	if _, err = fail2(); err != nil {
-		log.Println("Why did this fail?")
+	step = 2
+	if _, err := failA(); err != nil { // lock at
+		fmt.Printf("%d:`failA()` -> Why did this fail?: %v\n", step, err)
 	} else {
-		log.Println("`fail2()` err is nil")
+		fmt.Printf("%d:`failA()` -> err: %v\n", step, err)
+	}
+
+	step = 3
+	if _, err = failB(); err != nil {
+		fmt.Printf("%d:`failB()` ->Why did this fail?: %v\n", step, err)
+	} else {
+		fmt.Printf("%d:`failB()` -> err: %v\n", step, err)
+	}
+
+	step = 4
+	if _, err := failB(); err != nil {
+		fmt.Printf("%d:`failB()` ->Why did this fail?: %v\n", step, err)
+	} else {
+		fmt.Printf("%d:`failB()` -> err: %v\n", step, err)
 	}
 }
