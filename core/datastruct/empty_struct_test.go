6273759454078b46e52996b0d38add302eac7667
18388871 <kozmo23g@gmail.com>
2020-07-08 16:25:39 +0300

    add empty struct test

---
+++ core/datastruct/empty_struct_test.go
@@ -0,0 +1,44 @@
+package datastruct
+
+import (
+	"fmt"
+	"github.com/magiconair/properties/assert"
+	"testing"
+	"unsafe"
+)
+
+func TestNotAllocateMemory(t *testing.T) {
+	a := struct{}{}
+	size := unsafe.Sizeof(a)
+	println(size)
+	assert.Equal(t, uintptr(0), size)
+}
+
+func TestImplementSet(t *testing.T) {
+	set := make(map[string]struct{})
+	for _, value := range []string{"apple", "orange", "apple"} {
+		set[value] = struct{}{}
+	}
+	fmt.Println(set)
+}
+
+func TestSignalToMain(t *testing.T) {
+	worker := func(ch chan struct{}) {
+		// Receive a message from the main program.
+		<-ch
+		println("worker")
+
+		// Send a message to the main program.
+		close(ch)
+	}
+
+	ch := make(chan struct{})
+	go worker(ch)
+
+	// Send a message to a worker.
+	ch <- struct{}{}
+
+	// Receive a message from the worker.
+	<-ch
+	println("end")
+}
