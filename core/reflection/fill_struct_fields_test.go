2b7237438bdd82546afe7b8a8c2d1720db0e73b7
18388871 <kozmo23g@gmail.com>
2020-07-13 16:37:30 +0300

    add reflection test

--- core/reflection/fill_struct_fields_test.go
+++ core/reflection/fill_struct_fields_test.go
@@ -81,3 +81,34 @@ func TestFillStructFieldFromInstancesMap(t *testing.T) {
 	assert.Equal(t, obj.I.getVal(), b)
 	assert.Equal(t, obj.Val, a, obj.Val)
 }
+
+func TestFillStructFieldFromInstancesMap_2(t *testing.T) {
+	m := make(map[string]interface{})
+
+	iib := injectedInterfaceImplB{}
+
+	assert.Equal(t, iib.getVal(), "")
+
+	vb := reflect.ValueOf(&iib)
+	vb.Elem().Field(0).Set(reflect.ValueOf(b))
+	m[b] = iib
+
+	assert.Equal(t, iib.getVal(), b)
+
+	obj := testReflectionStruct{}
+	val := reflect.ValueOf(&obj)
+	fmt.Println(val)
+	for i := 0; i < val.Elem().NumField(); i++ {
+		switch val.Elem().Field(i).Type() {
+		case typeOfInterface:
+			injection := m[b]
+			val.Elem().Field(i).Set(reflect.ValueOf(injection))
+		case typeOfString:
+			val.Elem().Field(i).SetString(a)
+
+		}
+	}
+	fmt.Println(val)
+	assert.Equal(t, obj.I.getVal(), b)
+	assert.Equal(t, obj.Val, a, obj.Val)
+}
