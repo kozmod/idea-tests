a13a98b1860b7e89a6860103d6966c34a64bba9a
18388871 <kozmo23g@gmail.com>
2020-07-14 15:03:48 +0300

    add bidirectional streaming client/server test

--- grpc/genproto.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-protoc -I ./proto --go_out=plugins=grpc:./vendor com/data/dataA.proto
-protoc -I ./proto --go_out=plugins=grpc:./vendor com/data/dataB.proto
-protoc -I ./proto --go_out=plugins=grpc:./vendor com/msg/msg.proto
--- grpc/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/kozmod/idea-tests/grpc
-
-go 1.13
