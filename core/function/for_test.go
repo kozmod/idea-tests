b2584e0a8b542317cb25196ed8247f159206fab2
18388871 <kozmo23g@gmail.com>
2020-09-25 08:17:49 +0300

    add fori test

---
+++ core/function/for_test.go
@@ -0,0 +1,20 @@
+package function
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestFor1(t *testing.T) {
+	s := []int{1, 2, 3, 4, 5, 6, 7}
+	var calls int
+	l := func(i []int) int {
+		leng := len(i)
+		calls++
+		return leng
+	}
+	for i := 0; i < l(s); i++ {
+
+	}
+	assert.Equal(t, calls, len(s)+1)
+}
