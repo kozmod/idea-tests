2c58d55896589eb3db6ac8c348eab9a066e1802d
18388871 <kozmo23g@gmail.com>
2020-10-08 16:52:30 +0300

    add error handling test

--- core/errors/error_test.go
+++ core/errors/error_test.go
@@ -2,9 +2,9 @@ package errors
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
 
-	"github.com/magiconair/properties/assert"
 	"github.com/pkg/errors"
 )
 
@@ -17,8 +17,14 @@ func (e *errorString) Error() string {
 	return e.msg
 }
 
-func newError() error {
+func newErrorString() error {
 	return &errorString{msg, st}
+}
+
+type errorOne struct{}
+
+func (e errorOne) Error() string {
+	return "Error One"
 }
 
 const (
@@ -26,10 +32,30 @@ const (
 	st  = "SOME_TEXT"
 )
 
-var qe = newError()
+var qe = newErrorString()
+
+func TestIs(t *testing.T) {
+	e := errors.New("a")
+	e2 := errors.New("a")
+	assert.False(t, errors.Is(e, e2))
+	assert.False(t, errors.Is(e2, e))
+	assert.False(t, e == e2)
+	assert.False(t, &e == &e2)
+
+	e3 := errorString{msg, st}
+	e4 := errorString{msg, st}
+	assert.False(t, errors.Is(&e3, &e4))
+	assert.True(t, e3 == e4) //equals by value
+
+	e5 := errorOne{}
+	e6 := fmt.Errorf("E2: %w", errorOne{})
+	assert.True(t, errors.Is(e6, e5))
+	assert.False(t, errors.Is(e5, e6))
+	assert.False(t, errors.Is(e6, &e5))
+}
 
 func TestSwitchError(t *testing.T) {
-	err := newError()
+	err := newErrorString()
 	var etp *errorString
 	switch {
 	case err == nil:
@@ -46,7 +72,7 @@ func TestSwitchError(t *testing.T) {
 }
 
 func TestAs(t *testing.T) {
-	err := newError()
+	err := newErrorString()
 	var es *errorString
 	if errors.As(err, &es) {
 		fmt.Printf("Err[msg: %s; val: %s]\n", es.msg, es.val)
