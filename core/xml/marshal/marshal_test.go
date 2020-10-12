83bb4616863a6d9f7bf60890557e2f02b032514b
18388871 <kozmo23g@gmail.com>
2020-10-12 19:22:17 +0300

    add xml parsing test

--- core/xml/marshal/marshal_test.go
+++ core/xml/marshal/marshal_test.go
@@ -3,6 +3,8 @@ package marshal
 import (
 	"encoding/xml"
 	"fmt"
+	"io"
+	"strings"
 	"testing"
 )
 
@@ -51,4 +53,30 @@ func Test2(t *testing.T) {
 
 	res, _ = xml.Marshal(n)
 	fmt.Println(string(res))
+}
+
+const commentXml = `<?xml version="1.0" encoding="UTF-8"?>
+<!-- This is a comment I cannot get -->
+<ServerConfig>
+  <!-- This is a comment I can get -->
+  <KeyStore>/tmp/test</KeyStore>
+</ServerConfig>`
+
+func TestDecodeComment(t *testing.T) {
+	dec := xml.NewDecoder(strings.NewReader(commentXml))
+	for {
+		tok, err := dec.Token()
+		if err != nil && err != io.EOF {
+			panic(err)
+		} else if err == io.EOF {
+			break
+		}
+		if tok == nil {
+			fmt.Println("token is nill")
+		}
+		switch toke := tok.(type) {
+		case xml.Comment:
+			fmt.Println(string(toke))
+		}
+	}
 }
