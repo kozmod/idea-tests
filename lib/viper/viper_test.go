e60d04558544df2a2c6909b297dd4331f0688f6a
18388871 <kozmo23g@gmail.com>
2020-10-29 10:58:45 +0300

    add viper tests

--- lib/viper/viper_test.go
+++ lib/viper/viper_test.go
@@ -1,13 +1,15 @@
 package viper
 
 import (
+	"fmt"
+	"github.com/davecgh/go-spew/spew"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/spf13/viper"
 )
 
@@ -45,5 +47,33 @@ func TestReadConfig(t *testing.T) {
 	spew.Dump(Config)
 	Config.T = Config.T * time.Minute
 	spew.Dump(Config)
-	//fmt.Println(Config)
+	fmt.Println(Config)
+}
+
+type ExecuteJidResp struct {
+	Info   []interface{}
+	Return []map[string]interface{}
+}
+
+var yaml = `info: []
+return:
+  - foo.com:
+      some_value:
+        1.1.1.1: true
+      some_value2:
+        hey: hi`
+
+func TestReadConfigFromString(t *testing.T) {
+	var Resp ExecuteJidResp
+	viper.SetConfigType("yml")
+
+	if err := viper.ReadConfig(strings.NewReader(yaml)); err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := viper.Unmarshal(&Resp); err != nil {
+		log.Fatalln(err)
+	}
+	spew.Dump(Resp)
+	fmt.Println(Resp)
 }
