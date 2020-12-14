1225063fbb8ff914025a0914225a2893e70f80b2
18388871 <kozmo23g@gmail.com>
2020-12-14 20:52:32 +0300

    add marge to slice algorithm + fix

--- algorithms/leetcode/linked_list_cycle_test.go
+++ algorithms/leetcode/linked_list_cycle_test.go
@@ -53,6 +53,22 @@ func hasCycle(head *ListNode) bool {
 	if head == nil {
 		return false
 	}
+	slow := head
+	fast := head.Next
+	for slow != fast {
+		if fast == nil || fast.Next == nil {
+			return false
+		}
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return true
+}
+
+func hasCycleReq(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
 	next := head.Next
 	for depth := 0; next != nil; depth++ {
 
