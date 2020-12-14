10380ee41a5257d522f590118efbaafdd1615c41
18388871 <kozmo23g@gmail.com>
2020-12-14 20:48:16 +0300

    add Linked List Cycle tests

---
+++ algorithms/leetcode/linked_list_cycle_test.go
@@ -0,0 +1,96 @@
+package leetcode
+
+import (
+	. "github.com/kozmod/idea-tests/algorithms/linkedlist"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+//Linked List Cycle
+//
+//Given head, the head of a linked list, determine if the linked list has a cycle in it.
+//There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+//Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+//
+//Return true if there is a cycle in the linked list. Otherwise, return false.
+//
+//Example 1:
+//Input: head = [3,2,0,-4], pos = 1
+//Output: true
+//Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+//
+//Example 2:
+//Input: head = [1,2], pos = 0
+//Output: true
+//Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+//
+//Example 3:
+//Input: head = [1], pos = -1
+//Output: false
+//Explanation: There is no cycle in the linked list.
+
+func Test_Linked_List_Cycle(t *testing.T) {
+	head := &ListNode{Val: 0}
+	last := &ListNode{
+		Val:  2,
+		Next: head,
+	}
+	mid := &ListNode{
+		Val:  1,
+		Next: last,
+	}
+	head.Next = mid
+	assert.True(t, hasCycle(head))
+
+	assert.False(t, hasCycle(nil))
+
+	assert.False(t, hasCycle(&ListNode{Val: 0}))
+
+	assert.False(t, hasCycle(&ListNode{Val: 0, Next: &ListNode{Val: 1}}))
+}
+
+func hasCycle(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+	next := head.Next
+	for depth := 0; next != nil; depth++ {
+
+		if find(head, next, depth) {
+			return true
+		}
+		next = next.Next
+	}
+	return false
+}
+
+func find(head, search *ListNode, depth int) bool {
+	current := head
+	for i := 0; depth > i; i++ {
+		if head == search {
+			return true
+		}
+
+		current = current.Next
+	}
+	return false
+}
+
+func hasCycleArray(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+	arr := make([]*ListNode, 0, 0)
+	next := head.Next
+	arr = append(arr, head)
+	for next != nil {
+		for _, node := range arr {
+			if node == next {
+				return true
+			}
+		}
+		arr = append(arr, next)
+		next = next.Next
+	}
+	return false
+}
