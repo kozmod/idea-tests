516d6438d056b61789c5b42776ef1b7c9f463e56
18388871 <kozmo23g@gmail.com>
2021-01-11 09:55:01 +0300

    fix tcp client

--- tcp-server-client/tcp-cliet/main.go
+++ tcp-server-client/tcp-cliet/main.go
@@ -1,13 +1,28 @@
 package main
 
-import "net"
+import (
+	"log"
+	"net"
+)
 
 func main() {
 	conn, err := net.Dial("tcp", "127.0.0.1:9999")
 	if err != nil {
 		panic(err)
 	}
+	defer func(conn net.Conn) {
+		err = conn.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(conn)
 
-	conn.Write([]byte("hello"))
-	conn.Write([]byte("world"))
+	_, err = conn.Write([]byte("hello"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = conn.Write([]byte("world"))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
