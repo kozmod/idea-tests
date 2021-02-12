ec10b6764e0e4da00111510876453f70e0786e4b
18388871 <kozmo23g@gmail.com>
2021-02-12 07:23:55 +0300

    add channel test

--- core/concurrent/channel/nil_chan_block_test.go
+++ core/concurrent/channel/nil_chan_block_test.go
@@ -1,6 +1,7 @@
 package channel
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
@@ -30,4 +31,37 @@ func TestFor_NilChannelBlocks(t *testing.T) {
 		assert.True(t, st)
 		break
 	}
+}
+
+func Test_NilChannelBlocks_Solve_WithSignalChannel(t *testing.T) {
+	doWork := func(done <-chan interface{}, strings <-chan string,
+	) <-chan interface{} { // 1
+		terminared := make(chan interface{})
+
+		go func() {
+			defer close(terminared)
+
+			for {
+				select {
+				case <-done: // 2
+					return
+				case s := <-strings:
+					fmt.Println(s)
+				}
+			}
+		}()
+		return terminared
+	}
+
+	done := make(chan interface{})
+	terminated := doWork(done, nil)
+
+	go func() { // 3
+		// Cancel the operation after 1 second
+		time.Sleep(1 * time.Second)
+		close(done)
+	}()
+
+	<-terminated //4
+	fmt.Println("Done.")
 }
