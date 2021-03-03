c3e0f55b6ba35c7805b0357425ede5934dfa2423
18388871 <kozmo23g@gmail.com>
2021-03-03 16:12:16 +0300

    fix xml Marshal custom map test

--- core/xml/marshal/marshal_map_test.go
+++ core/xml/marshal/marshal_map_test.go
@@ -8,47 +8,59 @@ import (
 
 type Person struct {
 	Id         string `xml:"id"`
-	AddressMap addressMap
+	AddressMap AddressMap
 }
 type Address struct {
 	Street string
 }
 
-type addressMap map[string]Address
+type AddressMap map[string]Address
 
-func (m addressMap) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
-	if err := e.EncodeToken(start); err != nil {
-		return err
-	}
-
+func (m AddressMap) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
 	for key, val := range m {
-		s := xml.StartElement{Name: xml.Name{Local: key}}
-		if err := e.EncodeElement(val, s); err != nil {
+		s := xml.StartElement{Name: xml.Name{Local: "address"}}
+		type customStruct struct {
+			Key    string `xml:"key"`
+			Street string `xml:"street"`
+		}
+		if err := e.EncodeElement(customStruct{
+			Key:    key,
+			Street: val.Street,
+		}, s); err != nil {
 			return err
 		}
 	}
-
-	return e.EncodeToken(start.End())
+	return nil
 }
 
 func Test_Struct_WIth_Map(t *testing.T) {
-	addressMap := make(map[string]Address)
-	addressMap["KEY_1"] = Address{
+	addressOne := Address{
 		Street: "Bangalore",
 	}
-	addressMap["KEY_2"] = Address{
-		Street: "MSK",
+	addressTwo := Address{
+		Street: "Paris",
 	}
+	addressMap := make(map[string]Address)
+	addressMap["101"] = addressOne
+	addressMap["102"] = addressTwo
 	person := &Person{
-		Id:         "PERSON_ID_202",
+		Id:         "202",
 		AddressMap: addressMap,
 	}
 
-	res, err := xml.Marshal(person)
-	if err != nil {
-		panic(err)
-	}
+	out, err := xml.MarshalIndent(person, " ", "  ")
+	assert.NoError(t, err)
 	assert.Equal(t,
-		"<Person><id>PERSON_ID_202</id><AddressMap><KEY_1><Street>Bangalore</Street></KEY_1><KEY_2><Street>MSK</Street></KEY_2></AddressMap></Person>",
-		string(res))
+		` <Person>
+   <id>202</id>
+   <address>
+     <key>101</key>
+     <street>Bangalore</street>
+   </address>
+   <address>
+     <key>102</key>
+     <street>Paris</street>
+   </address>
+ </Person>`,
+		string(out))
 }
